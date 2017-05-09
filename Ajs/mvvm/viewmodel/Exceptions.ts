feb860921069b63f5b6438f9f42b8548f526a485
Frantisek Novak <frantisek.novak@atomsoftwarestudios.com>
2017-05-10 01:29:17 +0200

    Exception moved to the library as it will be shared with web workers so no need reference anymore

--- Ajs/mvvm/viewmodel/Exceptions.ts
+++ Ajs/mvvm/viewmodel/Exceptions.ts
@@ -21,8 +21,6 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 IN THE SOFTWARE.
 **************************************************************************** */
 
-///<reference path="../../Exception.ts" />
-
 namespace Ajs.MVVM.ViewModel {
 
     "use strict";
